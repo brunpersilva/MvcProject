ef69ebaf9ec36598c3f4ef3c7af631540befe2b0
Bruno Silva <brunpersilva@gmail.com>
2020-12-10 16:25:37 -0300

    Delete Seller Funtion

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -10,7 +10,7 @@ using SalesWebMvc.Services;
 namespace SalesWebMvc.Controllers
 {
     public class SellersController : Controller
-        
+
     {
         private readonly SellerService _sellerService;
         private readonly DepartmentService _departmentService;
@@ -40,6 +40,33 @@ namespace SalesWebMvc.Controllers
             _sellerService.Insert(seller);
             return RedirectToAction(nameof(Index));
         }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            _sellerService.Remove(id);
+            return RedirectToAction(nameof(Index));
+        }
+
+
+
+
 
 
     }
